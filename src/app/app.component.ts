cec5e5d0a12bfbf9864d57ac44eb8e1eb4971b61
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-07 16:16:33 -0400

    test bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-// import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Smoothie-On-The-Go';
-// }
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
