1c39bac96c67f4aecc3a0a53d20bffbc4d1d1181
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-09-09 19:41:25 -0400

    hardcode a recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('Test', 'This is simply a test', 'https://cdn.shopify.com/s/files/1/2479/2098/products/Saphir_EN_2048x2048.jpg?v=1595266518'),
+
+  ];
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
